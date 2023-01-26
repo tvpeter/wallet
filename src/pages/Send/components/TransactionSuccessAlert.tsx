12e6b9232ae8d9d5144b887cb7afa9517dc63f3d
tvpeter <withtvpeter@gmail.com>
2023-01-26 09:28:57 +0100

    change network to testnet

--- src/pages/Send/components/TransactionSuccessAlert.tsx
+++ src/pages/Send/components/TransactionSuccessAlert.tsx
@@ -28,7 +28,7 @@ const TransactionSuccessAlert = ({ txid }: Props) => {
           <div className="mt-4">
             <div className="-mx-2 -my-1.5 flex">
               <a
-                href={`https://blockstream.info/tx/${txid}`}
+                href={`https://blockstream.info/testnet/tx/${txid}`}
                 rel="noreferrer"
                 target="_blank"
                 className="bg-green-50 px-2 py-1.5 rounded-md text-sm font-medium text-green-800 hover:bg-green-100 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-green-50 focus:ring-green-600"
